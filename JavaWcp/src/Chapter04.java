0bff60eadb3bd45db4486f2317f38659a5268bee
KouOsada <kouosada.0930@gmail.com>
2022-01-22 21:38:14 +0900

    [fix]

--- JavaWcp/src/Chapter04.java
+++ JavaWcp/src/Chapter04.java
@@ -2,7 +2,7 @@
 public class Chapter04 extends Variable01 {
 	public static void main(String[] args) {
 		final int LUCKY_NUMBER = 8;
-		String str = "長田　耕";
+		String str = "park hyeonjoon";
 		System.out.println(str);
 
 		str = "KPOP";
