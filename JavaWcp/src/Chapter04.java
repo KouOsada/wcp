7f1ee1c4a49c7cf74c536bb30e2ba3e5b4e2a980
KouOsada <kouosada.0930@gmail.com>
2022-01-22 21:34:11 +0900

    [Add]Javaの6章まで

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 extends Variable01 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 8;
+		String str = "長田　耕";
+		System.out.println(str);
+
+		str = "KPOP";
+		System.out.println(str);
+
+		System.out.println(LUCKY_NUMBER);
+	}
+}
